726e8adb7b3d9059b424e7bf3bab1811a711260e
maxime-manicourt <maxime.manicourt@gmail.com>
2018-07-26 02:42:38 +0200

    ajout attributs models

--- db/migrate/20180726003743_create_users.rb
+++ db/migrate/20180726003743_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.belongs_to :course
       t.timestamps
     end
   end
